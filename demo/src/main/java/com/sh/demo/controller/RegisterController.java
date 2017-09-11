ac2bf5829edd79630d2701e6679b32410017ad9b
alwaysalong <1316084833@qq.com>
2017-09-11 19:58:39 +0800

    Merge branch 'master' of https://github.com/alwaysalong/growtProcess

--- demo/src/main/java/com/sh/demo/controller/RegisterController.java
+++ demo/src/main/java/com/sh/demo/controller/RegisterController.java
@@ -12,14 +12,10 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sh.demo.commons.ConstantsClass;
 import com.sh.demo.service.IUserInfoService;
-import com.sh.demo.service.impl.UserInfoServiceImpl;
 
 @Controller
 @RequestMapping("/register")
