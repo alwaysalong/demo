b681b2ac6e20150d7b2d37c04a24835a86066fa9
alwaysalong <1316084833@qq.com>
2017-11-08 18:01:28 +0800

    排序算法

---
+++ pri-web/src/main/java/com/sh/pri/sort/HeapSort.java
@@ -0,0 +1,15 @@
+package com.sh.pri.sort;
+
+/**
+ * 选择排序—堆排序（Heap Sort）
+ * @author admin
+ * 第一次从下标为0的开始下标为0的这个数与后面的n-1个进行比较；找出最小或者最大的放在下标为0的这个位置；第二次从下标为1的开始比较；查询剩下的最大或者最小值；放在 
+ * 下标为1的位置；以此类推；直到排序完成
+ */
+public class HeapSort {
+
+	public static void main(String[] args) {
+		int [] a = {54,23,22,87,12,27,5,1};
+		
+	}
+}
