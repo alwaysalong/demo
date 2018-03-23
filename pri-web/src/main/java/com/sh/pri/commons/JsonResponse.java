ba5175f586489df36fd4bec09588c04221835ef3
alwaysalong <1316084833@qq.com>
2018-03-23 14:00:37 +0800

    封装json返回结构

---
+++ pri-web/src/main/java/com/sh/pri/commons/JsonResponse.java
@@ -0,0 +1,72 @@
+package com.sh.pri.commons;
+
+/**
+ * 封装统一的json响应结构
+ *      {
+ *          "meta": {
+ *               "success": true,
+ *               "message": "ok"
+ *          },
+ *          "data": ...
+ *       }
+ */
+public class JsonResponse {
+
+    private static final String OK = "ok";
+    private static final String ERROR = "error";
+
+    private Meta meta;
+    private Object data;
+
+    public JsonResponse success() {
+        this.meta = new Meta(true, OK);
+        return this;
+    }
+
+    public JsonResponse success(Object data) {
+        this.meta = new Meta(true, OK);
+        this.data = data;
+        return this;
+    }
+
+    public JsonResponse failure() {
+        this.meta = new Meta(false, ERROR);
+        return this;
+    }
+
+    public JsonResponse failure(String message) {
+        this.meta = new Meta(false, message);
+        return this;
+    }
+
+    public Meta getMeta() {
+        return meta;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public class Meta {
+
+        private boolean success;
+        private String message;
+
+        public Meta(boolean success) {
+            this.success = success;
+        }
+
+        public Meta(boolean success, String message) {
+            this.success = success;
+            this.message = message;
+        }
+
+        public boolean isSuccess() {
+            return success;
+        }
+
+        public String getMessage() {
+            return message;
+        }
+    }
+}
