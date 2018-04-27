fce32f5bd0d7f70e6cff55ef6bc5865215951213
alwaysalong <1316084833@qq.com>
2018-04-27 15:56:42 +0800

    微信服务器代码架子

---
+++ wx-app-server/src/main/java/com/example/demo/WxAppServerApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WxAppServerApplication.class, args);
+	}
+}
---
+++ wx-app-server/src/main/java/com/example/demo/com/example/demo/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.example.demo.com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by admin on 2018/4/27.
+ */
+@Controller
+public class TestController {
+
+    @RequestMapping("/index")
+    public String index(){
+
+        return "hello";
+    }
+}
